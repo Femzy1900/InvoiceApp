fd2af21bf454bfe5741330bf5236b7720b454d85
Femi Adedokun <adedokunfemi14@gmail.com>
2023-09-08 14:51:35 -0700

    workimg with firebase

---
+++ src/views/InvoiceView.vue
@@ -0,0 +1,52 @@
+<template>
+    <div v-if="currentInvoice" class="invoice-view container">
+        <router-link class="nav-link flex" :to="{name: 'Home'}">
+            <img src="../assets/icon-arrow-left.svg" alt="">
+            Go Back
+        </router-link>
+        <div class="header flex">
+            <div class="left flex">
+                <span>Status</span>
+                <div class="status-button flex" :class="{paid : currentInvoice.invoicePaid, draft: currentInvoice.invoiceDraft, pending: currentInvoice.invoicePending}">
+                <span v-if="invoice.invoicePaid">
+                    Paid
+                </span>
+                <span v-if="invoice.invoiceDraft">
+                    Draft
+                </span>
+                <span v-if="invoice.invoicePending">
+                    Pending
+                </span>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { mapMutations, mapState } from 'vuex';
+export default {
+    name: "invoiceView",
+    data() {
+        return {
+            currentInvoice: null,
+        }
+    },
+    created() {
+        this.getCurrentInvoice();
+    },
+    methods: {
+        ...mapMutations(["SET_CURRENT_INVOICE"]),
+
+        getCurrentInvoice() {
+            this.SET_CURRENT_INVOICE(this.$route.params.invoiceId)
+            this.currentInvoice = this.currentInvoiceArray[0];
+        }
+    },
+    computed: {
+        ...mapState(["currentInvoiceArray"])
+    }
+}
+</script>
+<style lang="">
+    
+</style>

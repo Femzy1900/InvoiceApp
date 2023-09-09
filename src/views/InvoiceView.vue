911108206f19b3e6c0b8962b5e7920d200579d24
Femi Adedokun <adedokunfemi14@gmail.com>
2023-09-09 05:56:43 -0700

    styling invoice

--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -34,7 +34,7 @@
 
         <div class="invoice-details flex flex-column">
             <div class="top flex">
-                <div class="left flex">
+                <div class="left flex flex-column">
                     <p><span>#</span> {{ currentInvoice.invoiceId }}
                     </p>
                     <p>{{ currentInvoice.productDescription }}</p>
@@ -71,8 +71,8 @@
                 </div>
             </div>
             <div class="bottom flex flex-column">
-                <div class="biling-items">
-                    <div class="heading flex">
+                <div class="billing-items">
+                    <div class="headings flex">
                         <p>Item Name</p>
                         <p>QTY</p>
                         <p>Price</p>
@@ -138,11 +138,10 @@ export default {
             }
         }
 
-        .header {
-            .invoice-details {
-                background-color: #1e2139;
-                border-radius: 20px;
-            }
+        .header,
+        .invoice-details  {
+            background-color: #1e2139;
+            border-radius: 20px;
             .header {
                 align-items: center;
                 padding: 24px 32px;
@@ -154,6 +153,142 @@ export default {
                     span {
                         color: #dfe3fa;
                         margin-right: 16px;
+                    }
+                }
+
+                .right {
+                    flex: 1;
+                    justify-content: flex-end;
+
+                    button {
+                        color: white;
+                    }
+                }
+
+
+            }
+
+            .invoice-details {
+                padding: 48px;
+                margin-top: 24px;
+
+                .top {
+                    div {
+                        color: #dfe3fa;
+                        flex: 1;
+                    }
+
+                    .left {
+                        font-size: 12px;
+                        p:first-child  {
+                            font-size: 24px;
+                            text-decoration: uppercase;
+                            color: white;
+                            margin-bottom: 8px;
+                        }
+                        p:nth-child(2) {
+                            font-size: 16px;
+                        }
+
+                        span {
+                            color: #888eb0;
+                        }
+                    }
+
+                    .right {
+                        font-size: 12px;
+                        align-items: flex-end;
+                    }
+                }
+
+                .middle {
+                    margin-top: 50px;
+                    color: #dfe3fa;
+                    gap: 16px;
+
+                    h4 {
+                        font-size: 12px;
+                        font-weight: 400;
+                        margin-bottom: 12px;
+                    }
+
+                    p {
+                        font-size: 16px;
+
+                    }
+
+                    .bill, .payment {
+                        flex: 1;
+                    }
+                    .payment {
+                        h4:nth-child(3) {
+                            margin-top: 32px;
+                        }
+
+                        p {
+                            font-weight: 600;
+                        }
+                    }
+
+                    .bill {
+                        p:nth-child(2) {
+                            font-size: 16px;
+                        }
+                        p:nth-child(3) {
+                            margin-top: auto;
+                        }
+                        p {
+                            font-size: 12px;
+                        }
+                    }
+
+                    .send-to {
+                        flex: 2;
+                    }
+                }
+
+                .bottom {
+                    margin-top: 50px;
+
+                    .billing-items {
+                        padding: 32px;
+                        border-radius: 20px 20px 0 0;
+                        background-color: #251945;
+
+                        .headings {
+                            color: #dfe3fa;
+                            font-size: 12px;
+                            margin-bottom: 32px;
+
+                            p:first-child {
+                                flex: 3;
+                                text-align: left;
+                            }
+
+                            p {
+                                flex: 1;
+                                text-align: right;
+                            }
+                        }
+
+                        .item {
+                            margin-bottom: 32px;
+                            font-size: 13px;
+                            color: white;
+
+                            &:last-child {
+                                margin-bottom: 0;
+                            }
+
+                            p:first-child {
+                                flex: 3;
+                                text-align: left;
+                            }
+                            p {
+                                flex: 1;
+                                text-align: right;
+                            }
+                        }
                     }
                 }
             }
